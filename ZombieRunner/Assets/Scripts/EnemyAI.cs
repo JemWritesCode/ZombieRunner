0bb2cc5203d6a349d02c6572a81b250358972964
Jem Cope <thejemmeh@gmail.com>
2023-01-30 09:20:57 -0800

    188 Enemies changed to Zombies and animated

--- ZombieRunner/Assets/Scripts/EnemyAI.cs
+++ ZombieRunner/Assets/Scripts/EnemyAI.cs
@@ -15,13 +15,21 @@ public class EnemyAI : MonoBehaviour
     float distanceToTarget = Mathf.Infinity;
     bool isProvoked = false;
 
+    EnemyHealth health;
+
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        health = GetComponent<EnemyHealth>();
     }
 
     void Update()
     {
+        if (health.IsDead())
+        {
+            enabled = false;
+            navMeshAgent.enabled = false;
+        }
         distanceToTarget = Vector3.Distance(target.position, transform.position);
         if (isProvoked)
         {
--- ZombieRunner/Assets/Scripts/EnemyHealth.cs
+++ ZombieRunner/Assets/Scripts/EnemyHealth.cs
@@ -6,7 +6,12 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] float hitPoints = 100f;
 
-    //create a public method which reduces hitpoints by the amount of damage
+    bool isDead = false;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
 
     public void TakeDamage(float damage)
     {
@@ -14,7 +19,14 @@ public class EnemyHealth : MonoBehaviour
         hitPoints -= damage;
         if(hitPoints <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        GetComponent<Animator>().SetTrigger("die");
     }
 }
